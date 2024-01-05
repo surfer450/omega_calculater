674887d9136613438003f6bbe714f5ab1f55859f
sagy <OmegaCollegeuser123@gmail.com>
2024-01-05 18:11:21 +0200

    digit calculater

--- calculater.py
+++ calculater.py
@@ -1,3 +1,7 @@
+from operators import Operator, CreateOperator
+from typing import List
+
+
 def is_operator(operator: str) -> bool:
     """
     this function finds if a string is an operator
@@ -21,18 +25,33 @@ def is_expression_valid(math_expr: str) -> bool:
     return True
 
 
-def is_priority_bigger(first_operator: str, second_operator: str) -> bool:
+def is_priority_higher(first_operator: Operator, second_operator: Operator) -> bool:
     """
     this function finds if first operator priority is bigger
-    :param first_operator: first operator as string
-    :param second_operator: second operator as string
+    :param first_operator: first operator as operator obj
+    :param second_operator: second operator as operator obj
     :return: true if first operator priority is bigger, else false
     """
-    power_dict = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "@": 5, "$": 5, "&": 5, "%": 4, "~": 6, "!": 6}
-    return power_dict[first_operator] > power_dict[second_operator]
+    return first_operator.get_priority() > second_operator.get_priority()
 
 
-def calculate_math_expression(math_expr: str):
+def execute_last_operator_in_operator_list(operator_list: List[Operator], operand_list: List[float]):
+    """
+    this function execute the last operator in operator list created by the calculate_math_expression function
+    :param operator_list: the list of operators in the math expression
+    :param operand_list: the list of operands in the math expression
+    :return: None
+    """
+
+    temp_operator_obj = operator_list[-1]
+    operands_lst_for_operator = []
+    for i in range(temp_operator_obj.get_number_of_operands()):
+        operands_lst_for_operator.append(operand_list.pop(len(operand_list) - 1))
+    operand_list.append(temp_operator_obj.calculate(operands_lst_for_operator))
+    operator_list.pop(len(operator_list) - 1)
+
+
+def calculate_math_expression(math_expr: str) -> float | None:
     """
     this function calculates math expressions as strings
     :param math_expr: math expression as string
@@ -41,18 +60,31 @@ def calculate_math_expression(math_expr: str):
     operand_list = []
     operator_list = []
 
-    for symbol in math_expr:
+    symbol_index = 0
+    while symbol_index < len(math_expr):
+        symbol = math_expr[symbol_index]
+
         if symbol.isnumeric():
             operand_list.append(float(symbol))
 
         elif is_operator(symbol):
-            operator_list.append(symbol)
+            operator_obj = CreateOperator.get_operator(symbol)
+            while len(operator_list) > 0 and is_priority_higher(operator_list[-1], operator_obj):
+                execute_last_operator_in_operator_list(operator_list, operand_list)
+            operator_list.append(operator_obj)
+
+        symbol_index += 1
+
+    while len(operator_list) > 0:
+        execute_last_operator_in_operator_list(operator_list, operand_list)
+
+    return operand_list[0]
 
 
 def main():
     math_expression = input("Enter expression: ")
     if is_expression_valid(math_expression):
-        calculate_math_expression(math_expression)
+        print(calculate_math_expression(math_expression))
 
 
 if __name__ == "__main__":
---
+++ operators.py
@@ -0,0 +1,239 @@
+from typing import List
+
+
+class Operator:
+    def __init__(self, priority: int, position: int, number_of_operands: int):
+        """
+        this is a constructor for a basic class representing an operator
+        :param priority: the integer priority of the operator in math expression
+        :param position: where is the operator positioned: 0 -between two operand,
+        -1 - in the left of an operand, 1- in the right of am operand
+        :param number_of_operands: number of operands this operator works on
+        """
+        self.priority = priority
+        self.position = position
+        self.number_of_operands = number_of_operands
+
+    def get_priority(self):
+        """
+        get the priority of the operator
+        :return: the int priority
+        """
+        return self.priority
+
+    def get_position(self):
+        """
+        get the position of the operator
+        :return: the int position
+        """
+        return self.position
+
+    def get_number_of_operands(self):
+        """
+        get the number of operands this operator works on
+        :return: the int number of operands
+        """
+        return self.number_of_operands
+
+    def calculate(self, list_of_operands: List[str]) -> float:
+        """
+        this function get operands and execute the operator on them
+        :param list_of_operands: list of operands in the expression
+        :return: the number after the execution of the operator
+        """
+        pass
+
+
+class Addition(Operator):
+    def __init__(self):
+        """
+        this is a constructor for addition: +
+        """
+        Operator.__init__(self, 1, 0, 2)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get two operands and add them
+        :param list_of_operands: list of operands in the expression
+        :return: the addition of the numbers
+        """
+        return list_of_operands[0] + list_of_operands[1]
+
+
+class Subtraction(Operator):
+    def __init__(self):
+        """
+        this is a constructor for subtraction: -
+        """
+        Operator.__init__(self, 1, 0, 2)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get two operands and subtract them
+        :param list_of_operands: list of operands in the expression
+        :return: the subtraction of the numbers
+        """
+        return list_of_operands[0] - list_of_operands[1]
+
+
+class Multiplication(Operator):
+    def __init__(self):
+        """
+        this is a constructor for multiplication: *
+        """
+        Operator.__init__(self, 2, 0, 2)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get two operands and multiply them
+        :param list_of_operands: list of operands in the expression
+        :return: the multiplication of the numbers
+        """
+        return list_of_operands[0] * list_of_operands[1]
+
+
+class Division(Operator):
+    def __init__(self):
+        """
+        this is a constructor for division: /
+        """
+        Operator.__init__(self, 2, 0, 2)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get two operands and dived them
+        :param list_of_operands: list of operands in the expression
+        :return: the division of the numbers
+        """
+        return list_of_operands[0] / list_of_operands[1]
+
+
+class Exponentiation(Operator):
+    def __init__(self):
+        """
+        this is a constructor for exponentiation: ^
+        """
+        Operator.__init__(self, 3, 0, 2)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get two operands and execute exponentiation on them
+        :param list_of_operands: list of operands in the expression
+        :return: the number after the execution of the operator
+        """
+        return list_of_operands[0] ** list_of_operands[1]
+
+
+class Average(Operator):
+    def __init__(self):
+        """
+        this is a constructor for average: @
+        """
+        Operator.__init__(self, 5, 0, 2)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get two operands and find the average of them
+        :param list_of_operands: list of operands in the expression
+        :return: the average of the numbers
+        """
+        return (list_of_operands[0] + list_of_operands[1]) / 2
+
+
+class Maximum(Operator):
+    def __init__(self):
+        """
+        this is a constructor for maximum: $
+        """
+        Operator.__init__(self, 5, 0, 2)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get two operands and find the bigger one
+        :param list_of_operands: list of operands in the expression
+        :return: the bigger number
+        """
+        if list_of_operands[0] > list_of_operands[1]:
+            return list_of_operands[0]
+        return list_of_operands[1]
+
+
+class Minimum(Operator):
+    def __init__(self):
+        """
+        this is a constructor for minimum: &
+        """
+        Operator.__init__(self, 5, 0, 2)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get two operands and find the smaller one
+        :param list_of_operands: list of operands in the expression
+        :return: the smaller number
+        """
+        if list_of_operands[0] < list_of_operands[1]:
+            return list_of_operands[0]
+        return list_of_operands[1]
+
+
+class Modulo(Operator):
+    def __init__(self):
+        """
+        this is a constructor for modulo: %
+        """
+        Operator.__init__(self, 4, 0, 2)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get two operands and find the remnant after division
+        :param list_of_operands: list of operands in the expression
+        :return: the remnant after division
+        """
+        return list_of_operands[0] % list_of_operands[1]
+
+
+class Negation(Operator):
+    def __init__(self):
+        """
+        this is a constructor for negation: ~
+        """
+        Operator.__init__(self, 6, -1, 1)
+
+    def calculate(self, list_of_operands: List[float]) -> float:
+        """
+        this function get operand and change its sign
+        :param list_of_operands: list of operands in the expression
+        :return: the operand with the opposite sign
+        """
+        return list_of_operands[0] * -1
+
+
+class Factorial(Operator):
+    def __init__(self):
+        """
+        this is a constructor for factorial: !
+        """
+        Operator.__init__(self, 6, 1, 1)
+
+    def calculate(self, list_of_operands: List[int]) -> int:
+        """
+        this function get operand and find its factorial
+        :param list_of_operands: list of operands in the expression
+        :return: the factorial of the operand
+        """
+        answer = 1
+        for number in range(1, list_of_operands[0] + 1):
+            answer *= number
+        return answer
+
+
+class CreateOperator:
+    """
+    this class is used for creating an operator object by a given string operator
+    """
+
+    @staticmethod
+    def get_operator(str_operator: str) -> Operator:
+        operators_dict = {"+": Addition, "-": Subtraction, "*": Multiplication, "/": Division, "^": Exponentiation,
+                          "@": Average, "$": Maximum, "&": Minimum, "%": Modulo, "~": Negation, "!": Factorial}
+        return operators_dict[str_operator]()
